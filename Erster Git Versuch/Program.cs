1671fd9a95acc65c5a120f96d9912dd4454daa2d
MarvinKock <marvin_kock@hotmail.de>
2015-11-23 08:48:35 +0100

    Initial Realease

---
+++ Erster Git Versuch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erster_Git_Versuch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Amelie");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
